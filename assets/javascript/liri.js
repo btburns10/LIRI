5bff045bda8799c0a1ab53111beb357445689f2b
Brendan Burns <btburns10@gmail.com>
2018-08-10 18:55:53 -0400

    functions updated

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,17 +1,21 @@
 require("dotenv").config();
-
+//set npm packages
 const keys = require("./keys.js");
 const Twitter = require("twitter");
 const Spotify = require("node-spotify-api");
+const request = require("request");
 
 //set keys
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 const apiKey = "trilogy";
 
-const command = process.argv[2];
+//set command line variables
+var command = process.argv[2];
+var query = "";
 const argv = process.argv;
 
+//switch function to execute command line entries
 switch(command) {
     case "my-tweets":
     tweetThis();
@@ -35,9 +39,9 @@ function tweetThis() {
     client.get('statuses/user_timeline', function(error, tweets, response) {
         if(!error) {
             for(let tweet of tweets) {
-                console.log(tweet.text);
-                console.log(tweet.created_at);
-                console.log(tweet.user.screen_name);
+                console.log(tweet.text,
+                    "\n" + tweet.created_at,
+                    "\n" + tweet.user.screen_name);
               }
           
         }
@@ -48,12 +52,9 @@ function tweetThis() {
 }
 
 function spotifyThis() {
-    var query = ""
-    var newQuery = query ? query : "purple rain"
-
-    for(var i = 3; i < argv.length; i++) {
-        query = query + argv[i];
-    }
+    
+    query = argv.slice(3).join(" ");
+    var newQuery = query ? query : "purple rain";
 
     spotify.search({ type: 'track', query: newQuery }, function(error, data) {
         if (!error) {
@@ -69,18 +70,24 @@ function spotifyThis() {
 }
 
 function movieThis() {
-    var movie = "";
-    //concatonate movie input
-    for(var i = 3; i < argv.length; i++) {
-        movie = movie + argv[i];
-    }
 
-    const url = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + movie;
-    console.log(url)
+    query = argv.slice(3).join("+");
+
+    const url = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + query;
+    
     request(url, function (error, response, body) {
       if(!error && response.statusCode === 200) {
         const movieData = JSON.parse(body);
-        console.log("Title: " + movieData.Title);
+        
+        console.log("Title: " + movieData.Title,
+            "\nYear: " + movieData.Year,
+            "\nBox Office: " + movieData.BoxOffice,
+            "\nIMDB Rating: " + movieData.Ratings[0].Value,
+            "\nRotten Tomatoes Rating: " + movieData.Ratings[1].Value,
+            "\nCountry: " + movieData.Country,
+            "\nLanguage: " + movieData.Language,
+            "\nPlot: " + movieData.Plot,
+            "\nActors: " + movieData.Actors);
       } else {
         console.log(error);
       }  
