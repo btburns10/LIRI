69a64aad6e9f7360e4c22263a33b33634e1fe8b5
Brendan Burns <btburns10@gmail.com>
2018-08-11 22:27:39 -0400

    deploy ready

--- assets/javascript/liri.js
+++ assets/javascript/liri.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
-//set npm packages
+//set npm and node packages 
 const keys = require("./keys.js");
 const Twitter = require("twitter");
 const Spotify = require("node-spotify-api");
 const request = require("request");
+const fs = require("fs");
 
 //set keys
 const spotify = new Spotify(keys.spotify);
@@ -12,7 +13,7 @@ const apiKey = "trilogy";
 
 //set command line variables
 var command = process.argv[2];
-var query = "";
+var query = process.argv[3];
 const argv = process.argv;
 
 //switch function to execute command line entries
@@ -21,7 +22,12 @@ switch(command) {
     tweetThis();
     break;
     case "spotify-this-song":
-    spotifyThis();
+    if(query) {
+        search = argv.slice(3).join(" ");
+    }else {
+        search =  "is this love";
+    }
+    spotifyThis(search);
     break;
     case "movie-this":
     movieThis();
@@ -42,38 +48,45 @@ function tweetThis() {
                 console.log(tweet.text,
                     "\n" + tweet.created_at,
                     "\n" + tweet.user.screen_name);
-              }
-          
-        }
-        else {
+                //append tweets to 'log.txt' file
+                fs.appendFile("../log.txt", tweet.text, function(error) {
+                    if(error) {
+                        console.log(error);
+                    }
+                })
+            }
+        }else {
             console.log(error);
         }
     });
 }
 
-function spotifyThis() {
+function spotifyThis(search) {
     
-    query = argv.slice(3).join(" ");
-    var newQuery = query ? query : "purple rain";
-
-    spotify.search({ type: 'track', query: newQuery }, function(error, data) {
+    spotify.search({ type: 'track', query: search }, function(error, data) {
         if (!error) {
           var track = (data.tracks.items[0]);
           console.log("Song Title: " + track.name);
           console.log("Artist: " + track.artists[0].name);
+          console.log("Album: " + track.album.name);
           console.log("Listen: " + track.external_urls.spotify);
-        }
-        else {
+          fs.appendFile("../log.txt", track.name + ", ", function(error) {
+            if(error) {
+                console.log(error);
+            }
+        })
+        }else {
             console.log(error);
         }
     })
 }
 
 function movieThis() {
-
+    //concatonate search movie title from command line input
     query = argv.slice(3).join("+");
+    var newQuery = query ? query : "Mr. Nobody";
 
-    const url = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + query;
+    const url = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=" + newQuery;
     
     request(url, function (error, response, body) {
       if(!error && response.statusCode === 200) {
@@ -88,12 +101,37 @@ function movieThis() {
             "\nLanguage: " + movieData.Language,
             "\nPlot: " + movieData.Plot,
             "\nActors: " + movieData.Actors);
-      } else {
+            //append searched movie name to 'log.txt' file
+            fs.appendFile("../log.txt", movieData.Title + ", ", function(error) {
+                if(error) {
+                    console.log(error);
+                }
+            })
+      }else {
         console.log(error);
       }  
     });
 }
 
 function doThis() {
-    
+    fs.readFile("../random.txt", "utf8", function(error, data) {
+        
+        if (error) {
+          return console.log(error);
+        }
+
+        console.log(data);
+        var dataArr = data.split(",");
+        var input1 = dataArr[0];
+        search = dataArr[1];
+        if(input1 === "spotify-this-song") {
+            spotifyThis(search);
+        }
+        if(input1 === "my-tweets") {
+            tweetThis();
+        }
+        if(input1 === "movie-this") {
+            movieThis(search);
+        }
+      });
 }
